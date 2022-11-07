aefa4cfdcfd6e6bd2fdcf11a4a85a4a3fc093462
aliher0 <alisertolgoev434@gmail.com>
2022-11-07 15:47:27 +0300

    lesson4

---
+++ main.js
@@ -0,0 +1,154 @@
+// DOM - Document Object Model
+// Git - github / gitlab
+
+
+
+
+//consele.log(document)
+
+
+
+// 0 1 - Двоичная система счисления 
+// 0 1  2 3  4 5 6 7 8 9 - Десятиричная система счисления
+// 0 1  2 3  4 5 6 7 8 9  a   b   c    d    e   f
+//                        10  11  12   13   14  15-
+// шестьнадцатиричная система счисления
+// hex-#ff ff ff / rgb-255 255 255
+
+
+// 57 = 5 * 10 + 7 = 57
+
+// ff = 15 * 16 + 15
+// 255(f f) = 15 * 16 + 10 =255
+
+
+
+
+// function Taketurns() {
+//     const HEX_MAX = 100;
+//     const HEX_MIN = 0;
+
+
+// const randNumber = Math.round (Math.Taketurns() * (HEX_MAX - HEX_MIN)+ HEX_MIN);
+// console.log(1)
+
+
+// var js_button = document.getElementById('color-bth');
+// console.log(js_button)
+//   colorTextElement.innerHTML = colorHex;
+//      document.body.style.background = colorHex
+//      var colorTextElement = wrapper_inner_element.getElementsByClassName('background-color')
+//    [0];
+//    return randNumber.toString(16).padStart(3,'0')
+// }
+
+//  function RandomGenerate() {
+//      const HEX_MAX = 255;
+//      const HEX_MIN = 0;
+
+//      const randNumber = Math.round (Math.random() * (HEX_MAX - HEX_MIN)+ HEX_MIN
+//      );
+//      console.log('Test')
+//     return randNumber.toString(16).padStart(2, '0'); // 500
+// }
+//  function ChangeColor() {
+//      var redHex = RandomGenerate();
+//      var greenHex = RandomGenerate();
+//      var blueHex = RandomGenerate();
+//       var colorHex = ['#', redHex,greenHex.blueHex].join("");
+//      var wrapper_inner_element = document.querySelector('.wrapper_inner');
+
+//       colorTextElement.innerHTML = colorHex;
+//      document.body.style.background = colorHex
+//      var colorTextElement = wrapper_inner_element.getElementsByClassName('background-color')
+//      [0];}
+// var js_button = document.getElementById('color-bth');
+// console.log(js_button)
+// var js_button = document.getElementById('color-bt');
+// console.log(js_button)
+
+//  js_button.addEventListener('click', ChangeColor);
+
+
+var countElement = document.getElementsByClassName('count')[0]
+var incBtn = document.getElementsByClassName('inc')[0]
+var decBth = document.getElementsByClassName('dec')[0]
+
+
+var myChislo
+
+
+
+
+count = 0;
+function increment () {
+    count++;
+    console.log(count);
+}
+
+increment ();
+increment ();
+increment ();
+
+
+
+
+count = 0;
+function minus () {
+    count--;
+    console.log(count);
+}
+
+
+minus ();
+minus ();
+minus ();
+minus ();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function RandomGenerate() {
+//     const HEX_MAX = 255;
+//     const HEX_MIN = 0;
+
+//     const randNumber = Math.round (Math.random() * (HEX_MAX - HEX_MIN)+ HEX_MIN
+//     );
+//     console.log('Test')
+//    return randNumber.toString(16).padStart(3, '0'); // 500
+// }
+//  function ChangeColor() {
+//     var redHex = RandomGenerate();
+//     var greenHex = RandomGenerate();
+//     var blueHex = RandomGenerate();
+//     var colorHex = ['#', redHex,greenHex.blueHex].join("");
+//     var wrapper_inner_element = document.querySelector('.wrapper_inner');
+    
+//     colorTextElement.innerHTML = colorHex;
+//     document.body.style.background = colorHex
+//     }   var colorTextElement = wrapper_inner_element.getElementsByClassName('background-color')
+//      [0];
+
+// var js_button = document.getElementById('color-bth');
+// console.log(js_button)
+
+// js_button.addEventListener('click', ChangeColor);
